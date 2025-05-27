cee4357a8ca842626e911ef6ee251fc7d22c7e86
JorgeDam26 <jorgehm260505@gmail.com>
2025-05-27 10:01:43 +0200

    exe-app

--- zeroGluten/zeroGluten/obj/Debug/net8.0-windows/view/Productos.g.i.cs
+++ zeroGluten/zeroGluten/obj/Debug/net8.0-windows/view/Productos.g.i.cs
@@ -183,13 +183,13 @@ namespace zeroGluten.view {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/zeroGluten;component/view/productos.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/zeroGluten;V1.0.0.0;component/view/productos.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\view\Productos.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -199,7 +199,7 @@ namespace zeroGluten.view {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
